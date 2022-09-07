98116122f9ffdff5d3dfe1e5110a71d971d5412a
Rohan Patel <82868610+rohan-patel@users.noreply.github.com>
2022-09-07 10:30:11 +0530

    .

--- trader/src/main/java/com/rohan/usecase/trader/ui/model/Trader.java
+++ trader/src/main/java/com/rohan/usecase/trader/ui/model/Trader.java
@@ -24,7 +24,6 @@ public class Trader {
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
 	private Timestamp createdAt;
 	
-	@JsonSetter(nulls=Nulls.AS_EMPTY)
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
 	private Timestamp updatedAt;
 
